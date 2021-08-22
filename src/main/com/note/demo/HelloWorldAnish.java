fda7635a0511f26f46faa8da74d9a5a5b80d0b56
DESKTOP-U2ICPAO\IIIT <mundrathianish96@gmail.com>
2021-08-22 10:51:44 +0530

    updated with a little change in the program and also added a new file.

--- src/main/com/note/demo/HelloWorldAnish.java
+++ src/main/com/note/demo/HelloWorldAnish.java
@@ -4,13 +4,13 @@ package com.note.demo;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class HelloWorld {
+public class HelloWorldAnish {
 
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
 
-    System.out.println("Hello, World done by Anish123! The current time is  " + dtf.format(now));
+    System.out.println("Hello, World done by Anish! The current time is  " + dtf.format(now));
   }
 
 }
